3315cf96ee97fd66f47e503f2abfc79a34c8fd25
Thon Ly <thonly@heartbank.ceo>
2022-12-14 17:06:49 -0800

    refactor

--- components/sw-main/sw-home/template.mjs
+++ components/sw-main/sw-home/template.mjs
@@ -2,7 +2,9 @@ const template = document.createElement("template");
 
 template.innerHTML = `
     <link rel="stylesheet" href="components/sw-main/sw-home/shadow.css">
-    <main>Home</main>
+    <main>
+        <h1>Trilogy</h1>
+    </main>
 `;
 
 export default template;
