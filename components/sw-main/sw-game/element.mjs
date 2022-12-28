9664b57d67b574337cd987d77cb817e5af1a42ef
Thon Ly <thonly@heartbank.ceo>
2022-12-27 19:08:40 -0800

    begin quiz component

--- components/sw-main/sw-game/element.mjs
+++ components/sw-main/sw-game/element.mjs
@@ -1,3 +1,4 @@
+import { FRONTEND } from '/global.mjs';
 import template from './template.mjs';
 
 class SwGame extends HTMLElement {
@@ -7,10 +8,19 @@ class SwGame extends HTMLElement {
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
-    render(course, c) {
-        this.shadowRoot.getElementById('course').textContent = course.capitalize();
-        this.shadowRoot.getElementById('chapter').textContent = `Chapter ${c}`;
+    async render(Course, c) {
+        const { YEAR_BEGAN, getData } = await import(`${FRONTEND}/global2.mjs`);
+        const { course, chapters } = await getData(`https://raw.githubusercontent.com/SiliconWat/${Course}-cohort/main/${YEAR_BEGAN}/Syllabus.json`);
+        const quizzes = await getData(`https://gist.githubusercontent.com/thonly/0a6a6dd684288d3963723f69d91cebe4/raw/c94a72f794e89646fa2137d84fe3117b56372c74/${Course}.json`);
+
+        this.shadowRoot.getElementById('course').textContent = course.title;
+        this.shadowRoot.getElementById('chapter').textContent = `Chapter ${c}: ${chapters[c - 1].title}`;
+        this.#render(quizzes[c]);
         this.style.display = 'block';
+    }
+
+    #render(quiz) {
+        console.log(quiz)
     }
 }
 
--- components/sw-main/sw-game/template.mjs
+++ components/sw-main/sw-game/template.mjs
@@ -2,11 +2,31 @@ const template = document.createElement("template");
 
 template.innerHTML = `
     <link rel="stylesheet" href="components/sw-main/sw-game/shadow.css">
-    <main>
+    <header>
         <h1 id="course"></h1>
         <h2 id="chapter"></h2>
-        Quiz
+    </header>
+    <main>
+        <section>
+            <h3>QUESTION <span id="current"></span> OF <span id="total"></span></h3>
+            <p>No Time Limit</p>
+        </section>
+        <section>
+            <p id="question"></p>
+            <ol></ol>
+        </section>
+        <section>
+            <button>Submit Answer => Correct / Wrong</button>
+            <button>Next => Skip Question</button>
+        </section>
     </main>
+    <footer>
+        <h3>Correct: <span id="correct"></span></h3>
+        <h3>Wrong: <span id="wrong"></span></h3>
+        <h3>Skipped: <span id="skipped"></span></h3>
+        <h2>Score: <span id="score"></span></h2>
+        <button>Restart Quiz</button>
+    </footer>
 `;
 
 export default template;
