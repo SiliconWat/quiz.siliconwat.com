8e5eb5352df29931e1e3fedbb4ad221c519407b5
Thon Ly <thonly@heartbank.ceo>
2023-01-05 11:52:35 -0800

    add quiz instructions

--- components/sw-main/sw-home/template.mjs
+++ components/sw-main/sw-home/template.mjs
@@ -3,8 +3,12 @@ const template = document.createElement("template");
 template.innerHTML = `
     <link rel="stylesheet" href="components/sw-main/sw-home/shadow.css">
     <main>
-        <h1>Trilogy</h1>
-        Quiz
+        <h1>Quiz Instructions</h1>
+        <p>There is <strong>no</strong> <em>time limit</em>.</p>
+        <p>You can <strong>retake</strong> the quizzes <em>as many times</em> as you like to earn the <em>maximum</em> <strong>SW Coins</strong> possible!</p>
+        <p>To <strong>collect</strong> your reward, first register your <em>profile</em> at <a href="https://heartbank.me">HeartBank.me</a></p>
+        <p><strong>SW Coins</strong> are minted on the <a href="https://heartbank.net/#siliconwat">Silicon Wat Blockchain</a>, a <em>sub</em> blockchain under the <a href="https://heartbank.net"><strong>HeartBank <em>Supra</em> Blockchain</strong></a>.</p>
+        <p>As such, your <strong>SW Coins</strong> can be <em>exchanged</em> or <em>transferred</em> within the <strong>HeartBank</strong> <em>ecosystem of blockchains</em> at any time.</p>
     </main>
 `;
 
